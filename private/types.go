b2df885107ab629ad8a47171862474fdb327274a
go-numb <gonumb66@gmail.com>
2022-03-24 05:19:44 +0900

    add type for websocket

--- private/types.go
+++ private/types.go
@@ -185,7 +185,7 @@ type FillsParam struct {
 }
 
 type FundingPaymentsResponse struct {
-	FundingPayments []FundingPayment `json:"fundingPayments`
+	FundingPayments []FundingPayment `json:"fundingPayments"`
 }
 
 type FundingPayment struct {
@@ -249,6 +249,29 @@ type TradingReward struct {
 
 type TradingRewardsParam struct {
 	Epoch int `json:"epoch,omitempty"`
+}
+
+type TransfersResponse struct {
+	Transfers []Transfer `json:"transfers"`
+}
+
+type Transfer struct {
+	Type            string    `json:"type"`
+	ID              string    `json:"id"`
+	ClientID        string    `json:"clientId"`
+	CreditAmount    string    `json:"creditAmount"`
+	CreditAsset     string    `json:"creditAsset"`
+	DebitAmount     string    `json:"debitAmount"`
+	DebitAsset      string    `json:"debitAsset"`
+	FromAddress     string    `json:"fromAddress"`
+	Status          string    `json:"status"`
+	ToAddress       string    `json:"toAddress"`
+	TransactionHash string    `json:"transactionHash"`
+	ConfirmedAt     time.Time `json:"confirmedAt"`
+	CreatedAt       time.Time `json:"createdAt"`
+}
+
+type TransfersParam struct {
 }
 
 func (o OrderQueryParam) ToParams() url.Values {
